41920cf6cac9a001a0561491533c8608ebca998c
Dennis Chandrian <Dennischandrian@gmail.com>
2021-03-03 14:25:33 +0700

    Completed Lesson 7.2

--- app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepTrackerFragment.kt
+++ app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepTrackerFragment.kt
@@ -111,11 +111,9 @@ class SleepTrackerFragment : Fragment() {
         binding.sleepList.adapter = adapter
 
         sleepTrackerViewModel.nights.observe(viewLifecycleOwner, Observer {
-            sleepTrackerViewModel.nights.observe(viewLifecycleOwner, Observer {
-                it?.let {
-                    adapter.data = it
-                }
-            })
+            it?.let {
+                adapter.submitList(it)
+            }
         })
 
         return binding.root
